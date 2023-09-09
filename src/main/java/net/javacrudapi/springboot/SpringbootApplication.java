bf7eae715d3edd3064978d2ad4a317814081c3b5
satyapsr13 <satyapsr13@gmail.com>
2023-09-10 02:14:26 +0530

    working on Employee Controller

--- src/main/java/net/javacrudapi/springboot/SpringbootApplication.java
+++ src/main/java/net/javacrudapi/springboot/SpringbootApplication.java
@@ -1,10 +1,14 @@
 package net.javacrudapi.springboot;
 
+import net.javacrudapi.springboot.model.Employee;
+import net.javacrudapi.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
 
@@ -13,4 +17,22 @@ public class SpringbootApplication {
 
     }
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Employee employee=new Employee();
+        employee.setFirstName("Satya ");
+        employee.setLastName("Prakash");
+        employee.setEmailId("satyapsr14@gmail.com");
+        employeeRepository.save(employee);
+        Employee employee1=new Employee();
+        employee1.setFirstName("Prem ");
+        employee1.setLastName("Jatt");
+        employee1.setEmailId("prem@gmail.com");
+        employeeRepository.save(employee1);
+    }
+
+//    employeeRepository.save()
 }
---
+++ src/main/java/net/javacrudapi/springboot/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package net.javacrudapi.springboot.controller;
+
+import net.javacrudapi.springboot.model.Employee;
+import net.javacrudapi.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees()
+    {
+        return  employeeRepository.findAll();
+
+    }
+
+
+}
